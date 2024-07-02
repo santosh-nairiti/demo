31c8c6e60e5a7026955612988c1cb0069f182349
santosh-nairiti <santosh@nairiti.com>
2024-07-02 20:08:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// document.getElementById("practice").innerHTML = "Hello";
+
+// function myFunction() {
+//     document.getElementById("example3").innerHTML = "paragraph changed!!";
+// }
+
+// document.getElementById("exp4").innerHTML = 5 + 6;
+
+// document.write(5 + 9);
