f537dd52db173e89b1832089d074d94b83702664
santosh-nairiti <santosh@nairiti.com>
2024-07-03 18:19:37 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ const gzip = require('gulp-gzip');
 const cleanCSS = require('gulp-clean-css');
 
 function minifyCSS() {
-    return gulp.src('src(1)/css/*.css')
+    return gulp.src('src/css/*.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(gulp.dest('dist/css'));
 }
 
 function minifyJS() {
-    return gulp.src('src(1)/js/*.js')
+    return gulp.src('src/js/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('dist/js'));
 }
