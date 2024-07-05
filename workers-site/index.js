d9508e3cfef46e4575fbcca1da0b9b2931fbcfd7
santosh-nairiti <santosh@nairiti.com>
2024-07-05 19:33:34 +0530

    Add files via upload

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,16 +1,31 @@
 addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
-  })
+    event.respondWith(handleRequest(event.request));
+  });
   
-  /**
-   * Respond to the request with the contents of index.html from the root directory
-   * @param {Request} request
-   */
   async function handleRequest(request) {
-    // Fetch the index.html file from the root directory
-    const htmlResponse = await fetch('https://demo-bf3.pages.dev/index.html')
+    // Log incoming request headers
+    console.log('Request Headers:', JSON.stringify([...request.headers]));
   
-    // Return the fetched HTML file as the response
-    return htmlResponse
+    // Fetch the asset
+    let response = await fetch(request);
+  
+    // Log response headers before modification
+    console.log('Original Response Headers:', JSON.stringify([...response.headers]));
+  
+    // Modify response headers
+    response = new Response(response.body, response);
+  
+    // Determine content type based on file extension
+    const url = new URL(request.url);
+    if (url.pathname.endsWith('.css')) {
+      response.headers.set('Content-Type', 'text/css');
+    } else if (url.pathname.endsWith('.js')) {
+      response.headers.set('Content-Type', 'text/javascript');
+    }
+  
+    // Log response headers after modification
+    console.log('Modified Response Headers:', JSON.stringify([...response.headers]));
+  
+    return response;
   }
   
